3342f2fd12860d20f8b82558792408c7b0ae5d34
Valentina <valentina@Valentinas-MacBook-Air.local>
2026-03-23 20:03:01 -0500

    Added endpoints to get and post the progress

--- exploration/urls.py
+++ exploration/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import has_progress
+from .views import has_progress, get_user_progress, save_progress
 
 urlpatterns = [
     path('has_progress/<int:user_id>/', has_progress, name='has_progress'),
+    path('progress/<int:user_id>/', get_user_progress),
+      path('progress/', save_progress),  
 ]
--- exploration/views.py
+++ exploration/views.py
@@ -10,3 +10,61 @@ def has_progress(request, user_id):
     """Verifica si el usuario tiene progresos guardados"""
     has_data = Progress.objects.filter(user_id=user_id).exists()
     return Response({'has_progress': has_data})
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Progress
+
+@api_view(['GET'])
+def get_user_progress(request, user_id):
+    """Retorna las carreras completadas por el usuario"""
+
+    progresses = Progress.objects.filter(user_id=user_id, state='done')
+
+    visited = [p.career.career_id for p in progresses]
+
+    return Response({
+        "visited": visited
+    })
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Progress
+from .serializers import ProgressSerializer
+
+
+@api_view(['POST'])
+def save_progress(request):
+    """Guarda progreso de una carrera"""
+
+    user_id = request.data.get("user_id")
+    career_id = request.data.get("career_id")
+    state = request.data.get("state", "done")
+    feedback = request.data.get("feedback")
+    progress = request.data.get("progress")
+
+    existing = Progress.objects.filter(
+        user_id=user_id,
+        career_id=career_id
+    ).first()
+
+    if existing:
+        return Response(
+            {"message": "Ya existe progreso para esta carrera"},
+            status=status.HTTP_200_OK
+        )
+
+    serializer = ProgressSerializer(data={
+        "user": user_id,
+        "career": career_id,
+        "state": state,
+        "feedback": feedback,
+        "progress": progress
+    })
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
