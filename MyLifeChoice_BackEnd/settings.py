7e24669b4226f49852726344af3ddf558571f74d
Valentina <valentina@Valentinas-MacBook-Air.local>
2026-05-19 11:17:49 -0500

    fixed cors

--- MyLifeChoice_BackEnd/settings.py
+++ MyLifeChoice_BackEnd/settings.py
@@ -18,17 +18,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2@n-0cf@2_=h#pvc7inmu2c$4e-cg*-m_x@9pz5vv0lai+6-xx'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['mylifechoice-backend.onrender.com', 'localhost', '127.0.0.1']
@@ -36,7 +31,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://mylifechoice.vercel.app/",
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
